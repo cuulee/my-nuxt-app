8b06b0d3852d7dbe71c0c9c8bfc3ddfd12ac4358
oNguyenHaPhan <nhoxvip95@gmail.com>
2018-05-01 14:59:07 +0700

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <nuxt/>
+    <my-footer/>
+  </div>
+</template>
+
+<script>
+  import Navbar from '../components/Navbar'
+  import MyFooter from '../components/Footer.vue'
+
+  export default {
+    components: {
+      Navbar,
+      MyFooter
+    }
+  }
+</script>
+
+<style>
+  html {
+    font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
+    height: 100%;
+  }
+
+  footer {
+    text-align: center;
+    padding: 30px 0;
+    background: #eee;
+  }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Phan, I’m a self taught software developer based in HaNoi, VietNam. I build modern applications for the web! 
+      </p>
+    </div>
+  </section>
+</template>
